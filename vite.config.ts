7812efa26fdf5cac423662766f837db551e6ecef
stella <timothy_fuentes@hotmail.com>
2026-05-22 15:03:46 -0700

    Moved Index

--- vite.config.ts
+++ vite.config.ts
@@ -25,9 +25,10 @@ export default defineConfig({
   },
   root: path.resolve(import.meta.dirname, "client"),
   build: {
+  base: '/rpl-ai-assessment/',   // ← add this line
     outDir: path.resolve(import.meta.dirname, "dist/public"),
     emptyOutDir: true,
-  },
+},
   server: {
     fs: {
       strict: true,
